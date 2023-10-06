d456f136caa9752e519dd52e94bc5c3b4747c705
AhmadEbraAlali <ahmadalali46589@gmail.com>
2023-10-06 21:37:19 +0300

    task5-3

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded" , function(){
+    fetch('.assets/all.json')
+    .then(response => response.json())
+    .then(data => {
+        const flag = document.querySelector()
+    })
+}) 
